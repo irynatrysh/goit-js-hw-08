cef026489b3233e2effff244e4633726b3caac45
irynatrysh <98460886+irynatrysh@users.noreply.github.com>
2023-10-06 21:38:33 -0400

    update all tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              alt="${item.description}"
+            />
+          </a>
+        </li>`
+    )
+    .join("");
+}
+
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    lightbox.close();
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector(".feedback-form");
+const storageKey = "feedback-form-state";
 
+let state = {};
+
+function saveFormState(e) {
+    state[e.target.name] = e.target.value.trim();
+    localStorage.setItem(storageKey, JSON.stringify(state));
+}
+
+function loadFormState() {
+    try {
+        const savedState = localStorage.getItem(storageKey);
+        if (savedState) {
+            state = JSON.parse(savedState);
+            Object.entries(state).forEach(([key, val]) => {
+                if (form.elements[key]) {
+                    form.elements[key].value = val;
+                }
+            });
+        }
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log(state);
+    localStorage.removeItem(storageKey);
+    evt.target.reset();
+}
+
+form.addEventListener("input", throttle(saveFormState, 500));
+form.addEventListener("submit", handleSubmit);
+loadFormState();
